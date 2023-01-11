1fe6b90ee51d038bcd8532d606b7625fd0b6635a
Petr Kim <ppeximo@gmail.com>
2023-01-11 12:32:24 +0300

    Added cutAverage

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] managers = new int[]{15, 10, 22, 25}; // Массив с продажами продаж.
+        int[] managers = new int[]{15, 10, 22, 25, 2, 44, 17}; // Массив с продажами продаж.
         SalesManager mng = new SalesManager(managers); //Вызов метода.
         System.out.println(mng.max()); // Вывод максимального значения продаж.
+
+        System.out.println(mng.cutAverage(managers)); // Выволит целую часть обрезанного среднего.
 
     }
 }
